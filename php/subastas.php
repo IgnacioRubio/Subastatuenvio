ec6d4ad5818cf2bb13a77c62248fcdab85d7d0b9
IgnacioRubio <ignaciorubio3@gmail.com>
2017-05-29 23:04:47 +0200

    New updates: finished + min change

--- php/subastas.php
+++ php/subastas.php
@@ -191,8 +191,7 @@
 
 									$horas_restantes = $registro['duracion'] - round((getdate()[0] - $registro['fecha_creacion']) / 60 / 60);
 
-									//if ($horas_restantes >= 0) {
-									if (true) {
+									if ($horas_restantes >= 0) {
 
 										echo "<tr>";
 
